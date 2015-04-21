7181a37a6c5f80d083efffbd35d04eef59ab950d
Jonathan Berkhahn <jaberkha@us.ibm.com>
2015-04-21 14:00:56 -0700

    added win_test.bat for reals this time

---
+++ bin/win_test.bat
@@ -0,0 +1,20 @@
+SET GOPATH=%CD%\gopath
+SET PATH=%PATH%;%CD%\gopath\bin
+
+cd gopath\src\github.com\cloudfoundry\cli
+
+SET GODEPSPATH=%CD%\Godeps\_workspace
+SET GOPATH=%GODEPSPATH%;%GOPATH%;
+SET PATH=%PATH%;%GODEPSPATH%\bin
+
+go get github.com/jteeuwen/go-bindata/... || exit /b 1
+go-bindata -pkg resources -ignore ".go" -o cf/resources/i18n_resources.go cf/i18n/resources/... cf/i18n/test_fixtures/... || exit /b 1
+
+powershell -command set-executionpolicy remotesigned || exit /b 1
+powershell .\bin\replace-sha.ps1 || exit /b 1
+
+go build -v -o %CF_EXE_NAME% ./main || exit /b 1
+
+go install github.com/onsi/ginkgo/ginkgo
+c:\Go\bin\go test -i ./cf/... ./generic/... ./testhelpers/...
+c:\Go\bin\go test -cover -v ./cf/... ./generic/... ./testhelpers/... ./main/...
