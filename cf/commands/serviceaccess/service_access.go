1c3a1a1df34b3d4890d83392a42968c3b94b1527
Liz Dahlstrom and Matthew Boedicker <edahlstrom@pivotal.io>
2015-05-21 11:10:55 -0700

    Updated cf service-access and cf service-brokers so that they only pass through the 403 error, rather than giving specific lookup information.
    [#91452714]

--- cf/commands/serviceaccess/service_access.go
+++ cf/commands/serviceaccess/service_access.go
@@ -105,7 +105,7 @@ func (cmd *ServiceAccess) Run(c *cli.Context) {
 
 	brokers, err := cmd.actor.FilterBrokers(brokerName, serviceName, orgName)
 	if err != nil {
-		cmd.ui.Failed(T("Failed fetching service brokers.\n{{.Error}}", map[string]interface{}{"Error": err}))
+		cmd.ui.Failed(T("{{.Error}}", map[string]interface{}{"Error": err}))
 		return
 	}
 	cmd.printTable(brokers)
