0c051d751a0d4c1d68de60525d8141655191b53f
Simon Leung <leungs@us.ibm.com>
2015-06-10 17:09:53 -0700

    DEBUG: win32 unit
    Signed-off-by: Mark St.Godard <markstgodard@gmail.com>

--- bin/win32_test.bat
+++ bin/win32_test.bat
@@ -1,3 +1,5 @@
+echo "IN win32_test.bat 1"
+
 SET GOPATH=%CD%\gopath
 SET PATH=%PATH%;%CD%\gopath\bin
 
@@ -7,14 +9,19 @@ SET GODEPSPATH=%CD%\Godeps\_workspace
 SET GOPATH=%GODEPSPATH%;%GOPATH%;
 SET PATH=%PATH%;%GODEPSPATH%\bin
 
+echo "IN win32_test.bat 2"
 go get github.com/jteeuwen/go-bindata/... || exit /b 1
+echo "IN win32_test.bat 3"
 go-bindata -pkg resources -ignore ".go" -o cf/resources/i18n_resources.go cf/i18n/resources/... cf/i18n/test_fixtures/... || exit /b 1
+echo "IN win32_test.bat 4"
 
 powershell -command set-executionpolicy remotesigned < NUL || exit /b 1
 powershell .\bin\replace-sha.ps1 < NUL || exit /b 1
 
+echo "IN win32_test.bat 5"
 go build -v -o %CF_EXE_NAME% ./main || exit /b 1
 
+echo "IN win32_test.bat 6"
 REM go install github.com/onsi/ginkgo/ginkgo || exit /b 1
 
 REM ginkgo -cover -r ./cf ./generic ./testhelpers ./main
--- ci/scripts/windows32-unit.bat
+++ ci/scripts/windows32-unit.bat
@@ -1,8 +1,11 @@
+echo "IN windowns32-unit.bat 1"
 set root=%CD%
 
 set /p VERSION=<version\number
 
+echo "IN windowns32-unit.bat 2"
 call gopath\src\github.com\cloudfoundry\cli\bin\win32_test.bat || exit /b 1
+echo "IN windowns32-unit.bat 3"
 
 cd %root%
 
